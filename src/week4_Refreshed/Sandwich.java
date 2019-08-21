2e9753ad5c9d850a68f50870e692b8932954e2cf
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-08-21 11:00:48 +1000

    Task 2 - question 1 done with JUnit test

---
+++ src/week4_Refreshed/Sandwich.java
@@ -0,0 +1,42 @@
+package week4_Refreshed;
+
+public class Sandwich {
+    private String ingredient;
+    private String breadType;
+    private double price;
+
+
+
+
+    Sandwich(String ingredient, String breadType, double price){
+        this.ingredient = ingredient;
+        this.breadType = breadType;
+        this.price = price;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String getBreadType() {
+        return breadType;
+    }
+
+    public void setBreadType(String breadType) {
+        this.breadType = breadType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+}
---
+++ src/week4_Refreshed/TestSandwich.java
@@ -0,0 +1,16 @@
+package week4_Refreshed;
+
+
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class TestSandwich {
+
+
+    @Test
+    public void test(){
+        Sandwich sandwich = new Sandwich("tuna","wheat",4.99);
+
+    }
+}
