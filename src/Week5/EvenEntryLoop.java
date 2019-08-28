def8516728189310f0b951e8877b8b42d38dd1cf
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-08-28 20:17:13 +1000

    Add EventEntryLoop program

---
+++ src/Week5/EvenEntryLoop.java
@@ -0,0 +1,22 @@
+package Week5;
+
+import java.util.Scanner;
+
+public class EvenEntryLoop {
+    public static void main(String[] args) {
+        final int sentinel = 999;
+
+        Scanner scanner = new Scanner(System.in);
+
+        int userInput = 0;
+
+        while (userInput != sentinel) {
+            System.out.println("Enter an even number or 999 to exit: ");
+            userInput = scanner.nextInt();
+            if (userInput % 2 == 0)
+                System.out.println("Good Job");
+            else if (userInput != sentinel)
+                System.out.println("Error: Odd number.");
+        }
+    }
+}
