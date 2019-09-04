217b70463c4b10e9e30c545b7b7a8ced86543af5
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-04 21:56:59 +1000

    Add CarCareChoice2

--- src/week6/CarCareChoice2.java
+++ src/week6/CarCareChoice2.java
@@ -2,23 +2,24 @@ package week6;
 
 import java.util.Scanner;
 
-public class CarCareChoice {
+public class CarCareChoice2 {
+
     public static void main(String[] args) {
         String[] availableService = {"oil change", "tire rotation", "battery check", "brake inspection"};
         int[] price = {25, 22, 15, 5};
 
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Select one service: \n");
-
+        System.out.println("Services:");
         for (String s : availableService) {
             System.out.println(s);
         }
 
+        System.out.println("\nEnter first three Characters of Services to select : ");
         String selectedService = userInput.nextLine();
 
         for (int x = 0; x < availableService.length; x++) {
 
-            if (availableService[x].equals(selectedService))
+            if (availableService[x].substring(0, 3).equals(selectedService.substring(0, 3)))
                 System.out.println("Price is: $" + price[x]);
         }
     }
