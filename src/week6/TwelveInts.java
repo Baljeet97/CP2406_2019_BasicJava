5c95fa0f5ac6cd890b62380d59e9d5b0cb096150
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-04 21:08:03 +1000

    Add TwelveInts

---
+++ src/week6/TwelveInts.java
@@ -0,0 +1,19 @@
+package week6;
+
+public class TwelveInts {
+
+    public static void main(String[] args) {
+
+
+        int[] arrayOfNums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+        for (int arrayOfNum : arrayOfNums) {
+            System.out.println(arrayOfNum);
+        }
+        System.out.println();
+        for (int j = arrayOfNums.length - 1; j >= 0; --j) {
+            System.out.println(arrayOfNums[j]);
+
+        }
+    }
+}
