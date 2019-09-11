22b8e539cefb858b45bd8edfa18df965364cad41
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-11 11:48:36 +1000

    Add Horse

---
+++ src/week7/Coding/Horse.java
@@ -0,0 +1,35 @@
+package week7.Coding;
+
+import java.awt.*;
+
+public class Horse {
+
+    private String name;
+    private String color;
+    private int birthYear;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+}
