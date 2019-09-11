9a9a53999c16ab090814bab35a090cad6e0f31bd
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-11 11:18:05 +1000

    Debug DebugTen4

---
+++ src/week7/Debugging/FixDebugCustomer.java
@@ -0,0 +1,18 @@
+package week7.Debugging;
+
+public class FixDebugCustomer {
+    private int idNumber;
+    private String name;
+    double creditLimit;
+
+    public FixDebugCustomer(int id, String name, double credit) {
+        idNumber = id;
+        this.name = name;
+        creditLimit = credit;
+    }
+
+    public void display() {
+        System.out.println("Customer #" + idNumber +
+                " Name: " + name + "\nCredit limit $" + creditLimit);
+    }
+}
