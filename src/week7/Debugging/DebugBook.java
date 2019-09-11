7d8afa5d171b3b1c6bb2df788d2030601530543d
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-11 10:49:51 +1000

    Debug DebugTen3

---
+++ src/week7/Debugging/DebugBook.java
@@ -0,0 +1,14 @@
+package week7.Debugging;
+
+public class DebugBook {
+    int pages;
+
+    public DebugBook(int pgs) {
+        pages = pgs;
+    }
+
+    public int getPages() {
+
+        return pages;
+    }
+}
---
+++ src/week7/Debugging/DebugTen3.java
@@ -0,0 +1,21 @@
+package week7.Debugging;
+
+// A PhoneBook is a Book with a city
+
+import javax.swing.*;
+
+import static java.lang.Integer.parseInt;
+
+public class DebugTen3 {
+    public static void main(String[] args) {
+        String area, inStr;
+        int pages;
+        area = JOptionPane.showInputDialog(null,
+                "Enter city for phone book");
+        inStr = JOptionPane.showInputDialog(null,
+                "Enter number of pages in " + area + "'s phone book");
+        pages = parseInt(inStr);
+        DebugPhoneBook pb = new DebugPhoneBook(pages, area);
+        pb.display();
+    }
+}
