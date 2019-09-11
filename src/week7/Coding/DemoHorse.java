f3d23e937fea5369fe506844686fb7bb54a6b368
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-11 11:48:57 +1000

    Add DemoHorse and RaceHorse

---
+++ src/week7/Coding/DemoHorse.java
@@ -0,0 +1,18 @@
+package week7.Coding;
+
+public class DemoHorse {
+
+    public static void main(String[] args) {
+
+        Horse horse = new Horse();
+        RaceHorse race = new RaceHorse();
+
+        horse.setName("Man o'War");
+        horse.setColor("Black");
+        horse.setBirthYear(1997);
+        race.setRacesCompeted(30);
+
+        System.out.println("Horse Name: " + horse.getName() + "\nColor: " + horse.getColor() + "\nYear of Birth: " + horse.getBirthYear() + "\nRaces Competed: " + race.getRacesCompeted());
+
+    }
+}
---
+++ src/week7/Coding/RaceHorse.java
@@ -0,0 +1,15 @@
+package week7.Coding;
+
+public class RaceHorse extends Horse {
+
+    private int racesCompeted;
+
+
+    public void setRacesCompeted(int racesCompeted) {
+        this.racesCompeted = racesCompeted;
+    }
+
+    public int getRacesCompeted() {
+        return racesCompeted;
+    }
+}
