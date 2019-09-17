eed92050959aa2aeb44edbd4b51de0a8980c640c
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-17 10:34:58 +1000

    ADD FixDebugEleven2

---
+++ src/week8/Debugging/DebugOceanLiner.java
@@ -0,0 +1,19 @@
+package week8.Debugging;
+
+public class DebugOceanLiner extends DebugBoat {
+    public DebugOceanLiner() {
+        super("ocean liner ");
+        setPassengers();
+        setPower();
+    }
+
+    @Override
+    public void setPassengers() {
+        super.passengers = 2400;
+    }
+
+    @Override
+    public void setPower() {
+        super.power = "four engines";
+    }
+}
