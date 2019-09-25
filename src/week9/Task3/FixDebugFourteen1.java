49524d154a9a73ad0c74d0ee6bf241a13a656bd3
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-25 14:05:45 +1000

    Add fixDebugFourteen1

---
+++ src/week9/Task3/FixDebugFourteen1.java
@@ -0,0 +1,19 @@
+package week9.Task3;
+// Creates a frame with a specified size
+// Twice as tall as wide
+// The size is a constructor argument
+
+import javax.swing.*;
+
+public class FixDebugFourteen1 extends JFrame {
+    private FixDebugFourteen1(int size) {
+        super("This is my frame");
+        setSize(size, size * 2);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        FixDebugFourteen1 frame = new FixDebugFourteen1(200);
+        frame.setVisible(true);
+    }
+}
